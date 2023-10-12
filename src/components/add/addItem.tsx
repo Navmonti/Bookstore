f7e2e75a9512e5ff07de71ea6242bada1c141961
Navmonti <navid.montazeripour@gmail.com>
2023-10-12 09:18:58 -0400

    add model edit modal delete modal

--- src/components/add/addItem.tsx
+++ src/components/add/addItem.tsx
@@ -1,9 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react'; 
 
-const AddItem = () => {
-
-    return <>AddItem</>
-
+interface AddItemProps {
+   
 }
+
+const AddItem: React.FC<AddItemProps> = ({  }) => {
+  const [title, setTitle] = useState('');
+  const [price, setPrice] = useState('');
+  const [category, setCategory] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleAddItem = () => { 
+
+    // Clear the input fields
+    setTitle('');
+    setPrice('');
+    setCategory('');
+    setDescription('');
+
+  };
+
+  return (
+    <>
+      <h2>Add a Book</h2>
+      <form>
+        <div className="form-group">
+          <label className='input-label'>Title:</label>
+          <input
+          className='input-field'
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label className='input-label'>Price:</label>
+          <input
+          className='input-field'
+            type="number"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label className='input-label'>Category:</label>
+          <input
+          className='input-field'
+            type="text"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label className='input-label'>Description:</label>
+          <textarea
+            
+            className='input-field'   
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required   
+          />
+        </div>
+        <div className="button-container">
+          <button type="button" onClick={handleAddItem}>
+            Add Book
+          </button> 
+        </div>
+      </form>
+    </>
+  );
+};
 
 export default AddItem;
--- src/components/edit/editItem.tsx
+++ src/components/edit/editItem.tsx
@@ -1,9 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react'; 
 
-const UpdateItem = () => {
-
-    return <>DeleteItem</>
-
+interface EditItemProps {
+   
 }
 
-export default UpdateItem;
+const EditItem: React.FC<EditItemProps> = ({  }) => {
+  const [title, setTitle] = useState('');
+  const [price, setPrice] = useState('');
+  const [category, setCategory] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleEditItem = () => { 
+
+    // Clear the input fields
+    setTitle('');
+    setPrice('');
+    setCategory('');
+    setDescription('');
+
+  };
+
+  return (
+    <>
+      <h2>Add a Book</h2>
+      <form>
+        <div className="form-group">
+          <label className='input-label'>Title:</label>
+          <input
+          className='input-field'
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label className='input-label'>Price:</label>
+          <input
+          className='input-field'
+            type="number"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label className='input-label'>Category:</label>
+          <input
+          className='input-field'
+            type="text"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label className='input-label'>Description:</label>
+          <textarea
+            
+            className='input-field'   
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required   
+          />
+        </div>
+        <div className="button-container">
+          <button type="button" onClick={handleEditItem}>
+            Add Book
+          </button> 
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default EditItem;
