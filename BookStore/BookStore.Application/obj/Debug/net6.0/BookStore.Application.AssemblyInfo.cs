e4f9541a99724e28e239388bb4012f9b6446be48
Navmonti <navid.montazeripour@gmail.com>
2024-09-08 14:39:52 -0400

    change front application path

--- BookStore/BookStore.Application/obj/Debug/net6.0/BookStore.Application.AssemblyInfo.cs
+++ BookStore/BookStore.Application/obj/Debug/net6.0/BookStore.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f8df166d76246b8882f823fb1ad9f04ea981890")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
